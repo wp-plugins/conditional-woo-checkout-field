5165e9b288915c7f04202ec42d41463213acab80
scott.deluzio <scott.deluzio@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-12 23:42:55 +0000

    Tagging version 1.0.5
    git-svn-id: https://plugins.svn.wordpress.org/conditional-woo-checkout-field/tags/1.0.5@1043780 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- conditional-woo-checkout-field.php
+++ conditional-woo-checkout-field.php
@@ -4,7 +4,7 @@
    Plugin URI: http://surpriseazwebservices.com/wordpress-plugins/conditional-woo-checkout-field/
    Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=N2TYAV69U9CP4
    Description: Display a custom field at checkout in your WooCommerce store if a certain product is in the customer's cart.
-   Version: 1.0.3
+   Version: 1.0.5
    Author: Scott DeLuzio
    Author URI: http://surpriseazwebservices.com
    License: GPL2
@@ -167,7 +167,7 @@ add_action('woocommerce_checkout_process', 'conditional_checkout_field_process')
  
 function conditional_checkout_field_process() {
     // Check if the field is required and set, if not then show an error message.
-    if (get_option('oizuled_conditional_fields_required') == 'yes') {
+    if ($check_in_cart == true && get_option('oizuled_conditional_fields_required') == 'yes') {
 		if ( !$_POST['conditional_field'] ) {
 			wc_add_notice( __( get_option('oizuled_conditional_fields_requiredtext') ), 'error' );
 		}
@@ -188,12 +188,12 @@ function conditional_checkout_field_update_order_meta( $order_id ) {
 /**
  * Add the field to order emails
  **/
-add_filter( 'woocommerce_email_order_meta_keys', 'conditional_order_meta_keys' );
+add_action( 'woocommerce_email_after_order_table', 'conditional_order_meta_keys', 15, 2 );
 
-function conditional_order_meta_keys() {
+function conditional_order_meta_keys($order) {
 	if (get_option('oizuled_conditional_fields_addemail') == 'yes') {
-		if (get_post_meta( get_the_ID(), get_option('oizuled_conditional_fields_title'))) {
-			echo '<strong>' . get_option('oizuled_conditional_fields_title') . '</strong><br />' . get_post_meta( get_the_ID(), get_option('oizuled_conditional_fields_title'), true) . '<br />';
+		if (get_post_meta( $order->id, get_option('oizuled_conditional_fields_title'), true )) {
+			echo '<br /><strong>' . get_option('oizuled_conditional_fields_title') . ':</strong><br />' . get_post_meta( $order->id, get_option('oizuled_conditional_fields_title'), true );
 		}
 	}
 }
